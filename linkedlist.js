b7bf457c599d7ac1be54565fcd6e873ea3e3a059
bhuvaneshwari-k <123533635+bhuvaneshwari-k@users.noreply.github.com>
2023-02-10 12:01:40 +0530

    Update linkedlist.js

--- linkedlist.js
+++ linkedlist.js
@@ -14,7 +14,7 @@ class LinkedList{
                     this.rootNode= new Node(value)
                     this.count++;
                 }else{
-                    var temp = this.rootNode;
+                    let temp = this.rootNode;
                     while(temp.nextNode!= null){
                         temp = temp.nextNode;
                     }
@@ -23,19 +23,19 @@ class LinkedList{
                 }
             }
         insertElement(value, index){
-            var newNode=new Node(value);
+            let newNode=new Node(value);
             if(index==0){
             console.log("After Insert an element at Start:")
-            var temp = this.rootNode;
+            let temp = this.rootNode;
             newNode.nextNode=temp;
             this.rootNode=newNode;
         }
         else{
             console.log("After Insert an element at Middle/Last:")
-            var temp = this.rootNode;
-            var count1=0;
+            let temp = this.rootNode;
+            let count1=0;
             while(temp!= null){
-                        var current = temp.nextNode;
+                        let current = temp.nextNode;
                         count1++;
                         if(count1==index){
                             temp.nextNode=newNode;
@@ -52,15 +52,15 @@ class LinkedList{
         removeElement(index){
             if(index==0){
                 console.log("After Delete an element at Start:")
-                var temp = this.rootNode;
+                let temp = this.rootNode;
                 this.rootNode=temp.nextNode;
             }
            else{
             console.log("After Delete an element at Middle/Last:")
-            var temp = this.rootNode;
-            var count1=0;
+            let temp = this.rootNode;
+            let count1=0;
             while(temp!= null){
-                        var prev = temp.nextNode;
+                        let prev = temp.nextNode;
                         count1++;
                         if(count1==index){
                             temp.nextNode=prev.nextNode;
@@ -74,7 +74,7 @@ class LinkedList{
     }
         
         printElements(){
-            var temp = this.rootNode;
+            let temp = this.rootNode;
             if(temp == null){
                 console.log("Is empty!")
             }else{
@@ -88,7 +88,7 @@ class LinkedList{
 }
 
 
-var list = new LinkedList()
+let list = new LinkedList()
 list.addElement(1);
 list.addElement(2);
 list.addElement(3);
